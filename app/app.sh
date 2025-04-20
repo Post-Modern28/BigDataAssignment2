3140d42b8612886e9b048ef30b6d7ead4d4d4105
Mikhail Romanov <m.romanov@innopolis.university>
2025-04-20 15:05:14 +0300

    fix: hidden symbols read

--- app/app.sh
+++ app/app.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 # Start ssh server
 service ssh restart 
-
+echo "Starting services..."
 # Starting the services
-bash start-services.sh
+sed -i 's/\r$//' start-services.sh
 
+
+echo "Creating venv..."
 # Creating a virtual environment
 python3 -m venv .venv
 source .venv/bin/activate
 
+echo "Installing requirements..."
 # Install any packages
 pip install -r requirements.txt  
 
+
+echo "Packing venv"
 # Package the virtual env.
 venv-pack -o .venv.tar.gz
 
+echo "Collecting data"
 # Collect data
-bash prepare_data.sh
+sed -i 's/\r$//' prepare_data.sh
 
+echo "Done preparing"
 
 # Run the indexer
-bash index.sh data/sample.txt
-
-# Run the ranker
-bash search.sh "this is a query!"
+#bash index.sh data/sample.txt
+#
+## Run the ranker
+#bash search.sh "this is a query!"
